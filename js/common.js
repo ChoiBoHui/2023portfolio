ee2c76f3d31e29f9bf0282cf37a2cf63400a7fe8
choibohui <123dfga@naver.com>
2023-07-09 18:42:37 +0900

    update

--- js/common.js
+++ js/common.js
@@ -4,37 +4,8 @@ $(function () {
         e.preventDefault();
     });
 
-    // 메인메뉴 클릭 이벤트
-    $('.nav .mainMenu').on('click', function () {
-        let mainMenu = $(this).closest('.mainMenu');
-        let li = $(this).closest('li');
-
-        // 붙이기
-        mainMenu.addClass('view');
-        li.find('.subMenu').addClass('view');
-        li.find('.subMenu ul li:first-child a').addClass('view');
-        // 지우기
-        li.siblings().find('.mainMenu').removeClass('view');
-        li.siblings().find('.subMenu').removeClass('view');
-        li.siblings().find('.subMenu a').removeClass('view');
-    });
-
-    // 서브메뉴 클릭 이벤트
-    $('.nav .subMenu ul a').on('click', function () {
-        let li = $(this).closest('li');
-
-        $(this).addClass('view');
-        li.siblings().find('a').removeClass('view');
-    });
-
-    // 해상도 1880px 이하, 메뉴 fix
-    $('.triggerBtn').on('click', function () {
-        $(this).toggleClass('fix');
-        $('header').toggleClass('fix');
-        $('footer').toggleClass('fix');
-    });
-
-    function menulist() {
+    // 각 서브메뉴와 section에 인덱스 번호 부여
+    function menuList() {
         let startIndex = 0;
 
         $('.subMenuList').each(function () {
@@ -49,13 +20,50 @@ $(function () {
             $(this).attr('data-index', index);
         });
     };
-    menulist();
+    menuList();
 
-    $('.subMenuList li').on('click', function () {
-        var index = $(this).attr('data-index');
-        console.log(index);
+    // 메인메뉴 클릭 이벤트
+    $('.nav .mainMenu').on('click', function () {
+        let li = $(this).closest('li');
+        let subMenuFirstLi = li.find('.subMenu ul li:first-child');
+        let index = subMenuFirstLi.attr('data-index');
+        let targetSection = $('.main > .container > section').eq(index);
+
+        // mainMenu 적용
+        $(this).addClass('view');
+        li.siblings().find('.mainMenu').removeClass('view');
+
+        // subMenu 적용
+        li.find('.subMenu').addClass('view');
+        li.siblings().find('.subMenu').removeClass('view');
+
+        // subMenu가 가진 첫번째 li 적용
+        li.siblings().find('.subMenu li').removeClass('view');
+        li.find('.subMenu ul li:first-child').addClass('view');
+
+        // 해당 section에 .view 클래스 추가
+        targetSection.addClass('view');
+
+        // 형제 section에는 .view 클래스 제거
+        $('.main > .container > section').not(targetSection).removeClass('view');
     });
 
+    // 서브메뉴 클릭 이벤트
+    $('.nav .subMenu .subMenuList li').on('click', function () {
+        // let li = $(this).closest('li');
+
+        $(this).addClass('view');
+        $(this).siblings().removeClass('view');
+    });
+
+    // 해상도 1880px 이하, 메뉴 fix
+    $('.triggerBtn').on('click', function () {
+        $(this).toggleClass('fix');
+        $('header').toggleClass('fix');
+        $('footer').toggleClass('fix');
+    });
+
+    // 부여된 인덱스 번호와 같은 section에 클레스 추가
     $('.subMenuList li').on('click', function () {
         const liIndex = $(this).attr('data-index');
 
@@ -66,19 +74,7 @@ $(function () {
         $('.main > .container > section[data-index!="' + liIndex + '"]').removeClass('view');
     });
 
+    // var heraindex = $('section.dashBoard').attr('data-index');
+    // console.log(heraindex);
 
-
-    var heraindex = $('section.dashBoard').attr('data-index');
-    console.log(heraindex);
-
-
-    // $('.tab_menu>li>a').on('click', function (e) {
-    //     e.preventDefault();
-    //     var idx = $(this).parent().index();
-    //     $('.tab_content>li').removeClass('on');
-    //     $('.tab_content>li').eq(idx).addClass('on');
-    //     $('.tab_menu>li').removeClass('on');
-    //     $(this).parent().addClass('on');
-    // });
-    // business End
 });
